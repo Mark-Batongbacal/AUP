efc24401852812973b74b3433cc8921549b71925
Mark-Batongbacal <batongbacalmark@gmail.com>
2026-08-21 18:07:58 +0800

    Make password OTP migration standalone

--- database/migrations/20260821_PasswordOtpPurpose.sql
+++ database/migrations/20260821_PasswordOtpPurpose.sql
@@ -3,7 +3,26 @@ SET XACT_ABORT ON;
 
 BEGIN TRANSACTION;
 
-IF COL_LENGTH(N'dbo.PasswordResetTokens', N'Purpose') IS NULL
+IF OBJECT_ID(N'dbo.PasswordResetTokens', N'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.PasswordResetTokens
+    (
+        PasswordResetTokenId UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_PasswordResetTokens_Id DEFAULT NEWSEQUENTIALID(),
+        UserId UNIQUEIDENTIFIER NOT NULL,
+        TokenHash NVARCHAR(128) NOT NULL,
+        Purpose NVARCHAR(20) NOT NULL CONSTRAINT DF_PasswordResetTokens_Purpose DEFAULT (N'Reset'),
+        ExpiresAt DATETIME2 NOT NULL,
+        ConsumedAt DATETIME2 NULL,
+        CreatedAt DATETIME2 NOT NULL CONSTRAINT DF_PasswordResetTokens_CreatedAt DEFAULT SYSUTCDATETIME(),
+        CONSTRAINT PK_PasswordResetTokens PRIMARY KEY (PasswordResetTokenId),
+        CONSTRAINT FK_PasswordResetTokens_UserProfiles
+            FOREIGN KEY (UserId) REFERENCES dbo.UserProfiles(UserId) ON DELETE CASCADE
+    );
+
+    CREATE INDEX IX_PasswordResetTokens_User ON dbo.PasswordResetTokens (UserId);
+    CREATE UNIQUE INDEX UX_PasswordResetTokens_TokenHash ON dbo.PasswordResetTokens (TokenHash);
+END
+ELSE IF COL_LENGTH(N'dbo.PasswordResetTokens', N'Purpose') IS NULL
 BEGIN
     ALTER TABLE dbo.PasswordResetTokens
         ADD Purpose NVARCHAR(20) NOT NULL
