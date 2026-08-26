73080765518d5dad19b93d394eb8b69c8d9fcd54
Stephware <pinacate.stephen@gmail.com>
2026-08-27 00:12:51 +0800

    fix(database): make reviewed coordinate migration batch-safe

--- database/migrations/20260827_TricyclePointSubmissionReviewedCoordinates.sql
+++ database/migrations/20260827_TricyclePointSubmissionReviewedCoordinates.sql
@@ -1,55 +1,63 @@
-IF OBJECT_ID(N'dbo.TricyclePointSubmissions', N'U') IS NOT NULL
+IF OBJECT_ID(N'dbo.TricyclePointSubmissions', N'U') IS NULL
 BEGIN
-    IF COL_LENGTH(N'dbo.TricyclePointSubmissions', N'AdminLatitude') IS NULL
-    BEGIN
-        ALTER TABLE dbo.TricyclePointSubmissions
-            ADD AdminLatitude DECIMAL(9,6) NULL;
-    END;
-
-    IF COL_LENGTH(N'dbo.TricyclePointSubmissions', N'AdminLongitude') IS NULL
-    BEGIN
-        ALTER TABLE dbo.TricyclePointSubmissions
-            ADD AdminLongitude DECIMAL(9,6) NULL;
-    END;
+    THROW 50001, 'dbo.TricyclePointSubmissions does not exist. Apply the tricycle submission foundation migration first.', 1;
 END;
-GO
 
-IF OBJECT_ID(N'dbo.TricyclePointSubmissions', N'U') IS NOT NULL
-   AND NOT EXISTS (
-       SELECT 1
-       FROM sys.check_constraints
-       WHERE name = N'CK_TricyclePointSubmissions_AdminLatitude')
+IF COL_LENGTH(N'dbo.TricyclePointSubmissions', N'AdminLatitude') IS NULL
 BEGIN
-    ALTER TABLE dbo.TricyclePointSubmissions
+    EXEC(N'
+        ALTER TABLE dbo.TricyclePointSubmissions
+        ADD AdminLatitude DECIMAL(9,6) NULL;
+    ');
+END;
+
+IF COL_LENGTH(N'dbo.TricyclePointSubmissions', N'AdminLongitude') IS NULL
+BEGIN
+    EXEC(N'
+        ALTER TABLE dbo.TricyclePointSubmissions
+        ADD AdminLongitude DECIMAL(9,6) NULL;
+    ');
+END;
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.check_constraints
+    WHERE name = N'CK_TricyclePointSubmissions_AdminLatitude'
+      AND parent_object_id = OBJECT_ID(N'dbo.TricyclePointSubmissions'))
+BEGIN
+    EXEC(N'
+        ALTER TABLE dbo.TricyclePointSubmissions
         ADD CONSTRAINT CK_TricyclePointSubmissions_AdminLatitude
         CHECK (AdminLatitude IS NULL OR AdminLatitude BETWEEN -90 AND 90);
+    ');
 END;
-GO
 
-IF OBJECT_ID(N'dbo.TricyclePointSubmissions', N'U') IS NOT NULL
-   AND NOT EXISTS (
-       SELECT 1
-       FROM sys.check_constraints
-       WHERE name = N'CK_TricyclePointSubmissions_AdminLongitude')
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.check_constraints
+    WHERE name = N'CK_TricyclePointSubmissions_AdminLongitude'
+      AND parent_object_id = OBJECT_ID(N'dbo.TricyclePointSubmissions'))
 BEGIN
-    ALTER TABLE dbo.TricyclePointSubmissions
+    EXEC(N'
+        ALTER TABLE dbo.TricyclePointSubmissions
         ADD CONSTRAINT CK_TricyclePointSubmissions_AdminLongitude
         CHECK (AdminLongitude IS NULL OR AdminLongitude BETWEEN -180 AND 180);
+    ');
 END;
-GO
 
-IF OBJECT_ID(N'dbo.TricyclePointSubmissions', N'U') IS NOT NULL
-   AND NOT EXISTS (
-       SELECT 1
-       FROM sys.check_constraints
-       WHERE name = N'CK_TricyclePointSubmissions_AdminCoordinatePair')
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.check_constraints
+    WHERE name = N'CK_TricyclePointSubmissions_AdminCoordinatePair'
+      AND parent_object_id = OBJECT_ID(N'dbo.TricyclePointSubmissions'))
 BEGIN
-    ALTER TABLE dbo.TricyclePointSubmissions
+    EXEC(N'
+        ALTER TABLE dbo.TricyclePointSubmissions
         ADD CONSTRAINT CK_TricyclePointSubmissions_AdminCoordinatePair
         CHECK (
             (AdminLatitude IS NULL AND AdminLongitude IS NULL)
             OR
             (AdminLatitude IS NOT NULL AND AdminLongitude IS NOT NULL)
         );
+    ');
 END;
-GO
