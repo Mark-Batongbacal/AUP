fd156e217cb32d185f0cc965fe5ab8ab4d312382
Mark-Batongbacal <batongbacalmark@gmail.com>
2026-09-02 03:41:54 +0800

    fix: quote pelias restore java opts

--- infra/ansible/scripts/pelias-snapshot-restore.sh
+++ infra/ansible/scripts/pelias-snapshot-restore.sh
@@ -49,7 +49,7 @@ docker run --detach --name "$temp" \
   --volumes-from "$es" \
   --volume "$stage:/snapshots" \
   --env discovery.type=single-node \
-  --env ES_JAVA_OPTS=-Xms512m -Xmx512m \
+  --env "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
   --ulimit nofile=65536:65536 \
   --ulimit memlock=-1:-1 \
   --cap-add IPC_LOCK \
